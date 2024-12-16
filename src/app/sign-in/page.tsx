813155930708bee8191100d68b163991713eca3f
jim parker <munkherdene.ts0706@gmail.com>
2024-12-16 16:19:38 +0800

    added min logo

--- src/app/sign-in/page.tsx
+++ src/app/sign-in/page.tsx
@@ -30,7 +30,7 @@ export default function SignIn() {
   };
 
   return (
-    <div className="flex min-h-screen bg-slate-800 ">
+    <div className="flex min-h-screen  bg-slate-800 ">
       <div className="flex flex-1 flex-col justify-center px-6 py-12 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
           <img
