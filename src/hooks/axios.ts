ddb0d3fcb6ef9997ecd72dd2ab2ed45966202a2b
jim parker <munkherdene.ts0706@gmail.com>
2024-12-17 17:02:15 +0800

    asd

--- src/hooks/axios.ts
+++ src/hooks/axios.ts
@@ -5,7 +5,7 @@ const axiosInstance = axios.create({
   baseURL:
     utga === true
       ? "http://localhost:8080/api"
-      : "http://157.245.197.227:8080/api",
+      : "https://7fbd-66-181-176-234.ngrok-free.app/api",
 });
 
 axiosInstance.interceptors.request.use(
