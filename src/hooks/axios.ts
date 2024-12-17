8abee6be653eb113e2e661d4b29b4d6001d10ae8
jim parker <mongolzkarigun@gmail.com>
2024-12-17 21:43:45 +0800

    added

--- src/hooks/axios.ts
+++ src/hooks/axios.ts
@@ -5,7 +5,7 @@ const axiosInstance = axios.create({
   baseURL:
     utga === true
       ? "http://localhost:8080/api"
-      : "http://178.128.98.157:8080/api",
+      : "https://burtgel.gai2.shop/api",
 });
 
 axiosInstance.interceptors.request.use(
